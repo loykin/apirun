6ae9c57272ce1e533c1c0d1efefb3f52c6f1fca9
LeeSuk <s2suk@hotmail.com>
2025-09-01 03:21:31 +0900

    Remove unused `SaveResponseBodyKey` context key from migration package

--- internal/migration/ctxkeys.go
+++
@@ -1,7 +0,0 @@
-package migration
-
-// ctxKey is an unexported custom type to avoid context key collisions (SA1029).
-type ctxKey string
-
-// SaveResponseBodyKey is a typed context key to toggle storing response bodies in the migration history.
-var SaveResponseBodyKey ctxKey = "apimigrate.save_response_body"
